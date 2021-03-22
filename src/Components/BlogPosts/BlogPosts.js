3080c02b61b47dfc7e22acdfc5aef85db1ae4b3e
pramit-marattha <pramitmarattha5497@gmail.com>
2021-03-22 22:47:26 +0545

    blogpost styling added

--- src/Components/BlogPosts/BlogPosts.js
+++ src/Components/BlogPosts/BlogPosts.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUserInput, setBlogData } from "../../features/userSlice";
+import "./BlogPosts.css";
 
 const BlogPosts = () => {
   const searchInput = useSelector(selectUserInput);
@@ -25,19 +26,20 @@ const BlogPosts = () => {
   }, [searchInput]);
   return (
     <div className="blogpost">
-      <h1 className="blogpost__head__section">BLOG</h1>
+      <h1 className="blogpost__head__section">Rotten Avocado</h1>
       {loading ? <h1 className="loading">loading....</h1> : ""}
       <div className="blog___posts">
         {blogPost?.articles?.map((blog) => (
           <a className="blog__card" target="_blank" href={blog.url}>
             <img src={blog.image} />
             <div>
+              <p>{blog.publishedAt}</p>
+
+              <h1>{blog.title}</h1>
+              <p>{blog.description}</p>
               <h4 className="blogpost__title">
                 <span>{blog.source.name}</span>
-                <span>{blog.publishedAt}</span>
               </h4>
-              <h1>{blog.title}</h1>
-              <p>{blog.descripton}</p>
             </div>
           </a>
         ))}
